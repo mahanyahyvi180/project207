e3ece54d9c751338504759e3e56e166b8afc7fea
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-14 10:56:23 +0330

    Add files via upload

---
+++ WindowsFormsApp76/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp76
+{
+    class cEmployee
+    {
+        public event delbig toobig;
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string Concatinate()
+        {
+            if ((FirstName + LastName).Length > 10)
+                toobig.Invoke();
+
+            return FirstName + ' ' + LastName;
+        }
+    
+    }
+}
